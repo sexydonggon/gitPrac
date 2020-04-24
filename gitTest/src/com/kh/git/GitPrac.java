e4fbe0a04c31f34b46bf89f7085acfab54cd8a60
user1 <user1@DESKTOP-O1B88OU>
2020-04-24 12:31:28 +0900

    12시반 난조원2

--- gitTest/src/com/kh/git/GitPrac.java
+++ gitTest/src/com/kh/git/GitPrac.java
@@ -6,6 +6,8 @@ public class GitPrac {
 		System.out.println("hello");
 		System.out.println("World");
 		System.out.println("thx");
+		System.out.println("난조원");
+		System.out.println("난조장");
 		//잘올라가게지
 	}
 
